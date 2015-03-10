afad5834adfccec189ff131db60de603f081b2ba
Matt M <mattm@inkwellusa.com>
2015-03-10 11:39:48 -0400

    finish opencart config and install database locally

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8888/ecc/admin/');
+define('HTTP_CATALOG', 'http://localhost:8888/ecc/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8888/ecc/admin/');
+define('HTTPS_CATALOG', 'http://localhost:8888/ecc/');
+
+// DIR
+define('DIR_APPLICATION', '/Applications/MAMP/htdocs/ecc/admin/');
+define('DIR_SYSTEM', '/Applications/MAMP/htdocs/ecc/system/');
+define('DIR_LANGUAGE', '/Applications/MAMP/htdocs/ecc/admin/language/');
+define('DIR_TEMPLATE', '/Applications/MAMP/htdocs/ecc/admin/view/template/');
+define('DIR_CONFIG', '/Applications/MAMP/htdocs/ecc/system/config/');
+define('DIR_IMAGE', '/Applications/MAMP/htdocs/ecc/image/');
+define('DIR_CACHE', '/Applications/MAMP/htdocs/ecc/system/cache/');
+define('DIR_DOWNLOAD', '/Applications/MAMP/htdocs/ecc/system/download/');
+define('DIR_UPLOAD', '/Applications/MAMP/htdocs/ecc/system/upload/');
+define('DIR_LOGS', '/Applications/MAMP/htdocs/ecc/system/logs/');
+define('DIR_MODIFICATION', '/Applications/MAMP/htdocs/ecc/system/modification/');
+define('DIR_CATALOG', '/Applications/MAMP/htdocs/ecc/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'inkwell');
+define('DB_PASSWORD', 'yMoyUHq!_YzO');
+define('DB_DATABASE', 'eccnew');
+define('DB_PREFIX', 'eccnew_');
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8888/ecc/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8888/ecc/');
+
+// DIR
+define('DIR_APPLICATION', '/Applications/MAMP/htdocs/ecc/catalog/');
+define('DIR_SYSTEM', '/Applications/MAMP/htdocs/ecc/system/');
+define('DIR_LANGUAGE', '/Applications/MAMP/htdocs/ecc/catalog/language/');
+define('DIR_TEMPLATE', '/Applications/MAMP/htdocs/ecc/catalog/view/theme/');
+define('DIR_CONFIG', '/Applications/MAMP/htdocs/ecc/system/config/');
+define('DIR_IMAGE', '/Applications/MAMP/htdocs/ecc/image/');
+define('DIR_CACHE', '/Applications/MAMP/htdocs/ecc/system/cache/');
+define('DIR_DOWNLOAD', '/Applications/MAMP/htdocs/ecc/system/download/');
+define('DIR_UPLOAD', '/Applications/MAMP/htdocs/ecc/system/upload/');
+define('DIR_MODIFICATION', '/Applications/MAMP/htdocs/ecc/system/modification/');
+define('DIR_LOGS', '/Applications/MAMP/htdocs/ecc/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'inkwell');
+define('DB_PASSWORD', 'yMoyUHq!_YzO');
+define('DB_DATABASE', 'eccnew');
+define('DB_PREFIX', 'eccnew_');
